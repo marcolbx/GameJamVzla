275190edb7c46807fe54a87d34f54ccb98294655
marcolbx <marcolbx@hotmail.com>
2019-08-24 23:40:11 -0400

    Enemy REd

--- GameJAM/Assets/Scripts/Enemies/RedEnemy.cs
+++ GameJAM/Assets/Scripts/Enemies/RedEnemy.cs
@@ -7,15 +7,66 @@ public class RedEnemy : Enemy
     private Vector2 targetX; // solo en la X position
     [SerializeField] private float minDistance = 4f;
     [SerializeField] private float normalSpeed = 1f;
+    private AudioSource audioSource;
+    private bool soundPlayed = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        target = PlayerManager.instance.player.transform;
+        targetX = new Vector2(target.position.x,this.transform.position.y);
+        animator.SetFloat("Hp",health);
+
     }
 
     // Update is called once per frame
     override protected void Update()
     {
         base.Update();
+        targetX = new Vector2(target.position.x,this.transform.position.y);
+        if(health > 0)
+        {
+            if(Vector2.Distance(this.transform.position,target.position) < minDistance)
+            {
+
+            transform.position = Vector2.MoveTowards(transform.position,targetX,speed * Time.deltaTime);
+            PursuitModeState();
+            }
+            else
+            {
+                ChillModeState();
+            }
+        }
+    }
+
+    void PursuitModeState()
+    {
+        if(soundPlayed == false)
+        audioSource.Play();
+        soundPlayed = true;
+        animator.speed = 1.0f;
+        animator.ResetTrigger("Idle");
+        animator.SetTrigger("Pursuit");
+        speed = 2f;
+        if(target.position.x < animator.transform.position.x)
+        {
+                animator.transform.eulerAngles = new Vector3(0,0,0);
+        }
+        
+        else
+        {
+
+                animator.transform.eulerAngles = new Vector3(0,180,0);
+        }
+    }
+
+    void ChillModeState()
+    {
+        soundPlayed = false;
+       // Debug.Log("ChillModeState");
+        animator.ResetTrigger("Pursuit");
+        speed = normalSpeed;
+        animator.SetTrigger("Idle");
+        animator.speed = 1.0f;
     }
 }
