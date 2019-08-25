330bfcd7056b44d55188a0c4f85a6f0456db9040
marcolbx <marcolbx@hotmail.com>
2019-08-24 23:31:31 -0400

    RED ENEMY IN PROCESS

---
+++ GameJAM/Assets/Scripts/Enemies/RedEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemy : Enemy
+{
+    private Vector2 targetX; // solo en la X position
+    [SerializeField] private float minDistance = 4f;
+    [SerializeField] private float normalSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    override protected void Update()
+    {
+        base.Update();
+    }
+}
