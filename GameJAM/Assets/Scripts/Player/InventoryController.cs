fa1cc3f9a1716b97f69201ff315f3fe2a96fe485
Ramiro Vargas <ramirovargas.dev@gmail.com>
2019-08-26 18:06:57 -0400

    First Modification to Inventory Controller

--- GameJAM/Assets/Scripts/Player/InventoryController.cs
+++ GameJAM/Assets/Scripts/Player/InventoryController.cs
@@ -5,35 +5,77 @@ using UnityEngine.UI;
 
 public class InventoryController : MonoBehaviour
 {
-    public Sprite glass;
-    private Image controlerImage;
+    public Sprite Yoyo;
+    public SpriteRenderer controlerImage;
     public bool equipedMask=false;
+    private GameObject maskObject;
+    private Animator animator;
+    private GameObject rightArm;
+    private GameObject weapon;
+    public Sprite[] LeftArms;
+    public Sprite[] RightArms;
+    public StatusController statusController;
     // Start is called before the first frame update
     void Start()
     {
-        controlerImage = GetComponent<Image>();
         controlerImage.enabled = false;
+        maskObject = GameObject.FindGameObjectWithTag("Mask");
+        maskObject.GetComponent<SpriteRenderer>().enabled=false;
+        animator = GetComponent<Animator>();
+        rightArm = GameObject.FindGameObjectWithTag("RightArm");
+        weapon =  GameObject.FindGameObjectWithTag("Weapon");
+        animator.SetBool("Sword",true);
+        statusController = GetComponent<StatusController>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F)){
+        if(statusController.stamina <= 0){
+            controlerImage.sprite = null;
+            controlerImage.enabled = false;
+            equipedMask=false;
+            maskObject.GetComponent<SpriteRenderer>().enabled=false;
+        }
+        if(statusController.stamina > 0 && equipedMask ==true){
+            statusController.stamina -=Time.deltaTime/10;
+        }
+        if(Input.GetKeyDown(KeyCode.F)&& statusController.stamina > 0){
             if(controlerImage.enabled == false){
                 controlerImage.enabled = true;
-                controlerImage.sprite = glass;
+                //controlerImage.sprite = mask;
                 equipedMask= true;
+                statusController.stamina -=0.1f;
+                maskObject.GetComponent<SpriteRenderer>().enabled=true;
             }
             else{
                 if(equipedMask==true){
                     controlerImage.sprite = null;
                     controlerImage.enabled = false;
+                    equipedMask=false;
+                    maskObject.GetComponent<SpriteRenderer>().enabled=false;
                 }
                 else{
-                    controlerImage.sprite = glass;
+                    //controlerImage.sprite = mask;
+                    equipedMask= true;
+                    maskObject.GetComponent<SpriteRenderer>().enabled=true;
                 }
             }
+        }else if(Input.GetKeyDown(KeyCode.Alpha1)){
+            rightArm.GetComponent<SpriteRenderer>().sprite = LeftArms[1];
+            weapon.GetComponent<SpriteRenderer>().enabled=true;
+            animator.SetBool("Sword",true);
+            animator.SetBool("Book",false);
         }
+        else if(Input.GetKeyDown(KeyCode.Alpha2)){
+            rightArm.GetComponent<SpriteRenderer>().sprite = LeftArms[0];
+            weapon.GetComponent<SpriteRenderer>().enabled=false;
+            animator.SetBool("Sword",false);
+            animator.SetBool("Book",true);
+        }
+    }
+    public bool GetPower(){
+        return equipedMask;
     }
 }
