94e5e5ec879b06e7ee4480d030e6975e32b45359
Braulio Picon <brauliopicon96@gmail.com>
2019-08-25 21:03:06 -0400

    Checkpoint
    Codificación y realización del Checkpoint del juego

---
+++ GameJAM/Assets/Scripts/Checkpoint/PlayerPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gameMaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameMaster = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gameMaster.lastCheckpointPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
