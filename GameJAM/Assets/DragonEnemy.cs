c87b14516d3acd615e205167bf20a40f2ecbdfde
marcolbx <marcolbx@hotmail.com>
2019-08-29 05:34:38 -0400

    Paloma & Dragon Ready

---
+++ GameJAM/Assets/DragonEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonEnemy : Enemy
+{
+    public GameObject projectile;
+    [SerializeField] private float timeToShoot = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+
+        if(health >0 && timeToShoot <0)
+        {
+            InstantiateProjectile();
+            timeToShoot =3f;                //Hay q cambiar esto si se cambia el 3 de arriba
+        }
+
+        timeToShoot -= Time.deltaTime;
+
+    }
+
+    void InstantiateProjectile()
+    {
+        Instantiate(projectile,transform.position,Quaternion.identity);
+    }
+}
---
+++ GameJAM/Assets/DragonProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonProjectile : MonoBehaviour
+{
+    public float speed =4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this,8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed, Camera.main.transform);
+    }
+}
